e3221d7d89ff595e8f5842b9883715b685e6682c
Sonali Sehgal <33109941+sonalisehgal77@users.noreply.github.com>
2024-07-24 07:10:31 -0400

    Updated a few easy codes- ReverseString and ReverseStringPart2

--- src/main/java/leetcode_java/easy/ReverseStringPart2.java
+++ src/main/java/leetcode_java/easy/ReverseStringPart2.java
@@ -5,39 +5,31 @@ import java.util.Arrays;
 public class ReverseStringPart2 {
 
 
-    public String reverseString(String s, int k) {
-                int left = 0;
-                int right = k - 1;
-                //if (right <= left) return;
-                //abcdef
+    public String reverseStr(String s, int k) {
+                // convert string to char array
                 char[] ch = s.toCharArray();
-//                int len = ch.length;
-                for(int i = 0; i<=right;i++) {
-                    char tmp = ch[left];
-                    if (left == k) {
-                        ch[left] = tmp;
+                // iterate over char array with k * 2 step
+                for (int i = 0; i < s.length() - 1; i += (k * 2)) {
+                    // declare left and right pointers
+                    int l = i, r = Math.min(i + k - 1, s.length() - 1);
+                    // swap chars in array with pointers
+                    while (l < r) {
+                        // store char int tmp variable
+                        char tmp = ch[l];
+                        // swap char from right to left pointer ad increment left pointer
+                        ch[l++] = ch[r];
+                        // assign tmp char that we store and increment right pointer
+                        ch[r--] = tmp;
                     }
-                    if (k < left) {
-                        break;
-                    } else if (k >= left) {
-                        ch[left] = ch[right];
-                        ch[right] = tmp;
-                        left++;
-                        //right--;
-                    }
-                    if (left == right) {
-                        ch[left] = ch[right];
-                        ch[right] = tmp;
-                        break;
-                    }
-
                 }
+                // construct string from char array and return it
                 return new String(ch);
+            }
 
-        }
+
     public static void main(String[] args) {
         ReverseStringPart2 reverseStringPart2 = new ReverseStringPart2();
 
-        System.out.println(reverseStringPart2.reverseString("abcdefg",2));
+        System.out.println(reverseStringPart2.reverseStr("abcdefg",2));
     }
 }
